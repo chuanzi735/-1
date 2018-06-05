68524eec4577aa7ae98f67ed8e5a33251f327ca2
Brendan Forster <github@brendanforster.com>
2018-06-05 11:51:55 +0930

    reproduce it with the old setup

--- app/webpack.repro.js
+++ app/webpack.repro.js
@@ -1,9 +1,19 @@
 'use strict'
 
 const path = require('path')
+const webpack = require('webpack')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-  mode: 'production',
+  mode: 'none',
+  plugins: [
+    new UglifyJsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
   entry: './out-repro/ui/lib/author-input.js',
   target: 'electron-renderer',
   output: {
